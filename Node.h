6f4971523ef8bf6fe09f1e3c6b37b0b8e82b709b
Spaqin <3garfield@gmail.com>
2016-12-12 20:24:50 +0100

    added first version of the algorithm

--- Node.h
+++ Node.h
@@ -23,12 +23,23 @@ class Node
         next{nullptr,nullptr,nullptr,nullptr},
         color(rand()%4),
         gCost(rand()%100),
-        hCost(rand()%100)
+        hCost(rand()%100),
+		priority(0),
+		passedRoute(0)
     {
 
     }
     Node *next[4];
+	int passedRoute;
+	int priority;
+
+
+	void setPriority(int priority){ this->priority = priority; }
+	void setPassedRoute(int passedRoute){ this->passedRoute = passedRoute; }
+	int getPriority(){ return this->priority; }
+	int getPassedRoute(){ return this->passedRoute; }
 	bool isDisconnected() { return next[0] == nullptr && next[1] == nullptr && next[2] == nullptr && next[3] == nullptr;}
+
     //parameters for node display//
     //Note: Text will draw only if color != NONE//
     int color;
