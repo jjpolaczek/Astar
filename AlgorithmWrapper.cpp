703a7ed3ee3860395ad7f1b6f77007cf5d8c0521
Jakub Polaczek <jj.polaczek@gmail.com>
2017-01-18 23:19:18 +0100

    Indentation

--- AlgorithmWrapper.cpp
+++ AlgorithmWrapper.cpp
@@ -2,57 +2,57 @@
 
 
 
-	AlgorithmWrapper::AlgorithmWrapper(Node* startNode, Node* goalNode, Display* display)
-	{
-		this->goalNode = goalNode;
-		this->display = display;
-		aStar = new AStar(startNode, goalNode);
-		startNode->setColor(Node::BLUE);
-		goalNode->setColor(Node::BLUE);
-	}
-	
-	void AlgorithmWrapper::steppedSimulation()
-	{
-		bool autoStep = false;
-		Display::Input input = display->Draw();
-		while(aStar->step() != goalNode)
-		{
-			aStar->getCurrentNode()->setColor(Node::RED);
-			//pentla oczekuje na kolejny krok w algorytmie
-		if(autoStep != true)
-        {
-			while(!display->IsExit())
-                {
-                    input = display->PollEvent();
-                    if(input == Display::STEP)
-                        break;
-                    else if(input == Display::AUTO)
-                    {
-                        autoStep = !autoStep;
-                        break;
-                    }
+AlgorithmWrapper::AlgorithmWrapper(Node* startNode, Node* goalNode, Display* display)
+{
+    this->goalNode = goalNode;
+    this->display = display;
+    aStar = new AStar(startNode, goalNode);
+    startNode->setColor(Node::BLUE);
+    goalNode->setColor(Node::BLUE);
+}
 
+void AlgorithmWrapper::steppedSimulation()
+{
+    bool autoStep = false;
+    Display::Input input = display->Draw();
+    while(aStar->step() != goalNode)
+    {
+        aStar->getCurrentNode()->setColor(Node::RED);
+        //pentla oczekuje na kolejny krok w algorytmie
+        if(autoStep != true)
+        {
+            while(!display->IsExit())
+            {
+                input = display->PollEvent();
+                if(input == Display::STEP)
+                    break;
+                else if(input == Display::AUTO)
+                {
+                    autoStep = !autoStep;
+                    break;
                 }
-		}
-			display->Draw();
-		}
-		drawPassedRoute();
-	}
-	
-	void AlgorithmWrapper::continousSimulation()
-	{
-		while(aStar->step() != goalNode)
-		{
-			aStar->getCurrentNode()->setColor(Node::RED);
-			display->Draw();
-		}
-		drawPassedRoute();
-	}
-	
-	void AlgorithmWrapper::drawPassedRoute()	//funkcja odtwarzająca i rysująca na planszy znalezioną drogę
-	{
-		std::vector<Node*> path = aStar->reconstructPath(aStar->getCameFrom(), aStar->getCurrentNode());
-		for( auto i = path.begin(); i != path.end(); i++ )
-			(*i)->setColor(Node::BLUE);
-		display->Draw();
-	}
+
+            }
+        }
+        display->Draw();
+    }
+    drawPassedRoute();
+}
+
+void AlgorithmWrapper::continousSimulation()
+{
+    while(aStar->step() != goalNode)
+    {
+        aStar->getCurrentNode()->setColor(Node::RED);
+        display->Draw();
+    }
+    drawPassedRoute();
+}
+
+void AlgorithmWrapper::drawPassedRoute()	//funkcja odtwarzająca i rysująca na planszy znalezioną drogę
+{
+    std::vector<Node*> path = aStar->reconstructPath(aStar->getCameFrom(), aStar->getCurrentNode());
+    for( auto i = path.begin(); i != path.end(); i++ )
+        (*i)->setColor(Node::BLUE);
+    display->Draw();
+}
