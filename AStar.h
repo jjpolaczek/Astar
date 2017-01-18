418136c6a7755e49287df82a4dfc1144bb4cc5bd
Jakub Polaczek <jj.polaczek@gmail.com>
2017-01-18 23:56:05 +0100

    Dodano diagram klas

--- AStar.h
+++ AStar.h
@@ -8,19 +8,17 @@
 
 class AStar
 {
-	private:
-		std::vector<Node*> closedSet;	//zbiór odwiedzonych
-		std::vector<Node*> openSet;		//zbiór otkrytych nieodwiedzonych
-		std::map<Node*,Node*> cameFrom;
-		Node* currentNode;
-		Node* startNode;
-		Node* goalNode;
 	public:
 		AStar(Node* startNode, Node* goalNode);	
 		Node* step();
 		std::vector<Node*> reconstructPath(std::map<Node*,Node*> cameFrom, Node* currentNode);	//funkcja odtwarzająca przebytą drogę
 		Node* getCurrentNode();
 		std::map<Node*,Node*> getCameFrom();
-
-
+    private:
+        std::vector<Node*> closedSet;	//zbiór odwiedzonych
+        std::vector<Node*> openSet;		//zbiór otkrytych nieodwiedzonych
+        std::map<Node*,Node*> cameFrom;
+        Node* currentNode;
+        Node* startNode;
+        Node* goalNode;
 };
